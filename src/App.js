e17e6b2b4efdecd595c87d4a3b5905c621cc0847
aQUIzoneHD <aquizonehd@Mac-mini-di-aQUIzoneHD.local>
2021-09-19 01:07:17 +0200

    First scss assets

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="grid-box">
-        <div>Blocco1</div>
-        <div>Blocco2</div>
-        <div>Blocco3</div>
-        <div>Blocco4</div>
+        <div className="grid__main grid">
+        <header className="grid__header grid">
+          <nav className="grid__navbar grid">
+            <ul className="grid__list grid">
+              <li className="grid__list--item"><button>item_1</button></li>
+              <li className="grid__list--item"><button>item_2</button></li>
+              <li className="grid__list--item"><button>item_3</button></li>
+              <li className="grid__list--item"><button>item_4</button></li>
+            </ul>
+          </nav>
+        </header>
+        <section className="grid__box grid">
+          <div className="grid__box--item grid">Blocco1</div>
+          <div className="grid__box--item grid">Blocco2</div>
+          <div className="grid__box--item grid">Blocco3</div>
+          <div className="grid__box--item grid">Blocco4</div>
+        </section>
+        <section className="grid__text grid">
+          <div className="grid__text--content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae illum accusantium quidem quis? Odit, nisi aliquam. Repudiandae voluptas voluptatum, fuga distinctio harum ad illo commodi alias excepturi quia accusamus fugiat.
+            Culpa fugiat excepturi officia? Iusto fugit incidunt itaque, id pariatur optio odio! Aperiam voluptatem maiores, sequi neque sint temporibus repellendus, consequuntur natus numquam doloremque, reprehenderit corrupti tenetur. Libero, repudiandae dicta!
+            Ab id est reiciendis? Soluta itaque accusamus, quod ad exercitationem amet at beatae dolore praesentium. Numquam ab iste tenetur recusandae reprehenderit porro, impedit beatae. Natus architecto soluta vero aperiam perferendis?
+          </div>
+        </section>
       </div>
     </div>
   );
